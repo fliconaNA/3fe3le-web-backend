c6b3a36fff9fd1f2d4193d1e28bfd810fa02c81b
fliconaNA <flicona@nativapps.co>
2020-01-03 08:51:39 -0500

    add faker lib

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,53 @@
 const express = require('express');
 const User = require('../models/user')
-
+const bc = require('bcrypt');
+const _ = require('underscore')
+const fkr = require('faker')
 const app = express();
 
 
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    ok: true,
-    msg: 'Users'
-  });
+app.get('/users', (req, res) => {
+
+  User.find({})
+    .exec( (err, users)=>{
+      if (err) {
+        return res.status(400).json({
+          ok:false,
+          err
+        });
+      }
+
+      res.status(200).json({
+        ok: true,
+        users
+      });
+    })
+
+  
 })
 
 app.post('/users', (req, res) => {
-  let body = req.body;
 
+  // let  name= fkr.name.findName();
+  // let  email= fkr.internet.email() ;
+  // let  password= bc.hashSync(fkr.internet.password(),10) ;
+  // let  role= 'USER_ROLE';
+
+  let body = req.body;
   let user = new User({
     name: body.name,
     email: body.email,
-    password: body.password,
-    role: body.role,
+    password: bc.hashSync(body.password,10) ,
+    role: body.role
 
+    // name,
+    // email,
+    // password,
+    // role
+  
   });
+  // console.log('faker user',user);
 
   user.save((err, userDB) => {
     if (err) {
@@ -33,16 +59,33 @@ app.post('/users', (req, res) => {
 
     res.status(201).json({
       ok: true,
-      user: userDB
+      userDB
     })
   })
 })
 
-app.put('/users', (req, res) => {
-  res.send('put users');
+app.put('/users/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['name','email','img','role','status']) ;
+
+  
+  User.findByIdAndUpdate( id, body, {new: true, runValidators:true}, (err, userDB) =>{
+
+    if (err) {
+      return res.status(400).json({
+        ok:false,
+        err
+      })
+    }
+    
+    res.json({
+      ok: true,
+      userDB
+    });
+  })
 })
 
-app.delete('/users', (req, res) => {
+app.delete('/users/:id', (req, res) => {
   res.send('delete users');
 })
 
